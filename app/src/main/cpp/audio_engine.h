f9b9eeb4f49b913f4a02ad69f502014632401407
martinb62kvmartin-web <martin.b62kvmartin@gmail.com>
2026-08-07 07:18:22 +0300

    Add sample struct and modify voice structure

--- app/src/main/cpp/audio_engine.h
+++ app/src/main/cpp/audio_engine.h
@@ -3,7 +3,15 @@
 #include <oboe/Oboe.h>
 #include <array>
 #include <atomic>
+#include <cstdint>
 #include <memory>
+#include <mutex>
+#include <vector>
+
+struct Sample {
+    std::vector<float> data;
+    double sampleRate = 44100.0;
+};
 
 class AudioEngine : public oboe::AudioStreamDataCallback {
 public:
@@ -13,6 +21,7 @@ public:
     bool start();
     void stop();
     void triggerPad(int padIndex);
+    bool loadSample(int padIndex, int fd);
 
     oboe::DataCallbackResult onAudioReady(
             oboe::AudioStream* stream,
@@ -25,14 +34,32 @@ private:
 
     struct Voice {
         std::atomic<bool> active{false};
-        std::atomic<double> amp{0.0};
-        std::atomic<double> frequency{440.0};
+        std::atomic<bool> resetRequest{false};
+        std::atomic<bool> hasNextSample{false};
+        std::atomic<int> type{0};
+
+        std::shared_ptr<const Sample> sample;
+        std::shared_ptr<const Sample> nextSample;
+        double pos = 0.0;
+
+        double amp = 0.0;
+        double decay = 0.9999;
         double phase = 0.0;
+        double phase2 = 0.0;
+        double freq = 0.0;
+        double freq2 = 0.0;
+        double prevNoise = 0.0;
+        uint32_t rng = 123456789u;
+        int age = 0;
     };
+
+    double renderVoice(Voice& voice);
+    double nextNoise(Voice& voice);
 
     std::shared_ptr<oboe::AudioStream> outputStream;
     std::array<Voice, kNumPads> voices;
-    std::array<double, kNumPads> frequencies{};
+    std::array<std::shared_ptr<const Sample>, kNumPads> samples;
+    std::mutex sampleMutex;
     double sampleRate = 48000.0;
     bool running = false;
 };
