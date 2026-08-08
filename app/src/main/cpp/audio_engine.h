5d187b7223f82855bab1afd30d1ff8f3d26471a8
martinb62kvmartin-web <martin.b62kvmartin@gmail.com>
2026-08-08 14:19:47 +0300

    Update audio_engine.h

--- app/src/main/cpp/audio_engine.h
+++ app/src/main/cpp/audio_engine.h
@@ -1,102 +1,24 @@
-#pragma once
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
 
-#include <oboe/Oboe.h>
-#include <array>
-#include <atomic>
-#include <cstdint>
-#include <memory>
-#include <mutex>
-#include <vector>
+    <application
+        android:allowBackup="true"
+        android:label="SP1200 Clone"
+        android:supportsRtl="true"
+        android:theme="@android:style/Theme.Material.NoActionBar">
 
-struct Sample {
-    std::vector<float> data;
-    double sampleRate = 44100.0;
-};
+        <activity
+            android:name=".MainActivity"
+            android:exported="true"
+            android:screenOrientation="portrait"
+            android:configChanges="orientation|screenSize|keyboardHidden|screenLayout">
 
-class AudioEngine : public oboe::AudioStreamDataCallback {
-public:
-    AudioEngine();
-    ~AudioEngine() override;
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
 
-    bool start();
-    void stop();
-    void triggerPad(int padIndex);
-    void padRelease(int padIndex);
-    void setGateMode(bool enabled);
-    void setPitchSemitones(double semitones);
-    bool loadSample(int padIndex, int fd);
+    </application>
 
-    void setSeqPlaying(bool playing);
-    void setSeqBpm(double bpm);
-    void setSeqSwing(double swing);
-    void setSeqMask(int padIndex, int mask);
-
-    void setLoopPoints(int padIndex, double startFrac, double endFrac);
-    void setLoopOn(int padIndex, bool enabled);
-    bool trimToLoop(int padIndex);
-    std::vector<float> getPeaks(int padIndex, int buckets);
-
-    oboe::DataCallbackResult onAudioReady(
-            oboe::AudioStream* stream,
-            void* audioData,
-            int32_t numFrames
-    ) override;
-
-private:
-    static constexpr int kNumPads = 8;
-    static constexpr int kSteps = 16;
-
-    struct Voice {
-        std::atomic<bool> active{false};
-        std::atomic<bool> resetRequest{false};
-        std::atomic<bool> hasNextSample{false};
-        std::atomic<bool> gateClosed{false};
-        std::atomic<int> type{0};
-
-        std::shared_ptr<const Sample> sample;
-        std::shared_ptr<const Sample> nextSample;
-        double pos = 0.0;
-
-        int padIndex = 0;
-        bool loopEnabled = false;
-        double loopStart = 0.0;
-        double loopEnd = 0.0;
-
-        double amp = 0.0;
-        double decay = 0.9999;
-        double phase = 0.0;
-        double phase2 = 0.0;
-        double prevNoise = 0.0;
-        uint32_t rng = 123456789u;
-        int age = 0;
-    };
-
-    double renderVoice(Voice& voice);
-    double nextNoise(Voice& voice);
-    void triggerVoice(int padIndex);
-    void fireStep(int step);
-
-    std::shared_ptr<oboe::AudioStream> outputStream;
-    std::array<Voice, kNumPads> voices;
-    std::array<std::shared_ptr<const Sample>, kNumPads> samples;
-    std::mutex sampleMutex;
-    std::atomic<bool> gateMode{false};
-    std::atomic<double> pitchRate{1.0};
-
-    std::atomic<bool> seqPlaying{false};
-    std::atomic<bool> seqRestart{false};
-    std::atomic<double> seqBpm{90.0};
-    std::atomic<double> seqSwing{0.0};
-    std::array<std::atomic<int>, kNumPads> seqMask{};
-    double totalFrames = 0.0;
-    double nextStepFrame = 0.0;
-    int seqStep = 0;
-
-    std::array<std::atomic<double>, kNumPads> loopStartFrac{};
-    std::array<std::atomic<double>, kNumPads> loopEndFrac{};
-    std::array<std::atomic<bool>, kNumPads> loopOn{};
-
-    double sampleRate = 48000.0;
-    double releaseFactor = 0.999;
-    bool running = false;
-};
+</manifest>
