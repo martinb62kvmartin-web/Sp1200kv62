0b6353d7f0ad6701b6f926c0812b98fa9d1d4bc2
martinb62kvmartin-web <martin.b62kvmartin@gmail.com>
2026-08-12 10:32:40 +0300

    Update gesture import statements in apply_patch.py

--- tools/apply_patch.py
+++ tools/apply_patch.py
@@ -5,30 +5,19 @@ PATH = "app/src/main/java/com/example/sp1200/MainActivity.kt"
 with io.open(PATH, "r", encoding="utf-8") as f:
     text = f.read()
 
-old = """                            if (isStart) {
-                                Box(
-                                    modifier = Modifier.fillMaxHeight().width(3.dp)
-                                        .background(Color.White)
-                                )
-                            }
-                        }
-                }"""
+old = """import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.forEachGesture
+import androidx.compose.foundation.gestures.awaitPointerEventScope"""
 
-new = """                            if (isStart) {
-                                Box(
-                                    modifier = Modifier.fillMaxHeight().width(3.dp)
-                                        .background(Color.White)
-                                )
-                            }
-                        }
-                    }
-                }"""
+new = """import androidx.compose.ui.input.pointer.awaitFirstDown
+import androidx.compose.ui.input.pointer.forEachGesture
+import androidx.compose.ui.input.pointer.awaitPointerEventScope"""
 
 if old in text:
     text = text.replace(old, new, 1)
-    print("Patched: brace restored")
+    print("Patched: gesture imports")
 else:
-    print("Skipped: brace anchor")
+    print("Skipped: gesture imports")
 
 with io.open(PATH, "w", encoding="utf-8") as f:
     f.write(text)
